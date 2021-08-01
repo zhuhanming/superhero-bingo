3d61cbe001af277388442fa836651b8a14526ebe
Hanming Zhu <hanming@huawen.com>
2021-08-01 11:46:09 +0800

    Add constants to shared

---
+++ shared/src/constants/socket.ts
@@ -0,0 +1,10 @@
+export const IO_CONNECT = 'connection';
+
+export const REQ_CREATE_BINGO = 'req_create_bingo';
+export const RES_CREATE_BINGO = 'res_create_bingo';
+export const REQ_UPDATE_BINGO = 'req_update_bingo';
+export const RES_UPDATE_BINGO = 'res_update_bingo';
+export const REQ_FETCH_BINGO = 'req_fetch_bingo';
+export const RES_FETCH_BINGO = 'res_fetch_bingo';
+
+export const SOCKET_DISCONNECT = 'disconnect';
