0823250d2b7ddf7e566e27f93c01f345c05c1e18
Hanming Zhu <hanming@huawen.com>
2021-08-01 19:07:29 +0800

    Remove code that causes issues

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,12 +7,4 @@ dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 const apiServer = new ApiServer();
 apiServer.initialize();
 
-const cleanUp = async (signal: string) => {
-  await apiServer.close();
-  process.kill(process.pid, signal);
-};
-
-const signals = ['SIGINT', 'SIGHUP', 'SIGQUIT', 'SIGTERM', 'uncaughtException'];
-signals.forEach((signal) => process.on(signal, cleanUp));
-
 export default apiServer;
