b70642f540fc8f31ba1be14f1def4c96eb806b5f
Hanming Zhu <hanming@huawen.com>
2021-08-01 19:12:16 +0800

    Update app with home and navbar

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useLocation,
+} from 'react-router-dom';
+
+import Navbar from 'components/navbar';
+import { ROOT } from 'constants/routes';
+import Home from 'routes/home';
 
 const App: React.FC = () => {
-  return <>Hello</>;
+  const { pathname } = useLocation();
+  return (
+    <div className="min-h-screen min-w-full font-regular bg-yellow px-8 lg:px-24 py-12 text-black">
+      {pathname !== ROOT && <Navbar />}
+      <Router>
+        <Switch>
+          <Route path={ROOT}>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
