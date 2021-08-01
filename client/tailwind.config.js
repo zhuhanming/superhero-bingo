338ec8b24ba13f76029e15bc351201ea46d9fe99
Hanming Zhu <hanming@huawen.com>
2021-08-01 19:09:22 +0800

    Add tailwind styling

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,28 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      red: {
+        DEFAULT: '#e4663f',
+      },
+      yellow: {
+        DEFAULT: '#f8d347',
+      },
+      black: {
+        DEFAULT: '#414141',
+      },
+      blue: {
+        DEFAULT: '#6fcbf7',
+      },
+    },
+    borderWidth: {
+      DEFAULT: '1px',
+      0: '0',
+      2: '2px',
+      4: '4px',
+      8: '8px',
+      12: '12px',
+    },
   },
   variants: {
     extend: {},
