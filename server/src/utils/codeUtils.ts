ef3e252ff7bb29b201e0c4d94ea6cc0185c5f3d4
Hanming Zhu <hanming@huawen.com>
2021-08-01 11:46:26 +0800

    Add utils for code generation

---
+++ server/src/utils/codeUtils.ts
@@ -0,0 +1,11 @@
+const characters =
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const charactersLength = characters.length;
+
+export const makeCode = (length: number): string => {
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
