e6167b7420e4cfa725186c1755cdbc925463d344
Hanming Zhu <hanming@huawen.com>
2021-08-09 02:02:04 +0800

    feat: add handling of game joining

--- server/src/controllers/gameController.ts
+++ server/src/controllers/gameController.ts
@@ -1,4 +1,5 @@
-import { Game } from 'shared';
+import { sign } from 'jsonwebtoken';
+import { CreatedBingo, Game, Superhero } from 'shared';
 
 import prisma from 'lib/prisma';
 import { makeCode } from 'utils/codeUtils';
@@ -35,4 +36,66 @@ export const createGame = async (ownerCode: string): Promise<Game> => {
       heroes: true,
     },
   });
+};
+
+export const joinGame = async (
+  joinCode: string,
+  name: string
+): Promise<{
+  game: Game;
+  bingo: CreatedBingo;
+  user: Superhero;
+  token: string;
+}> => {
+  const game = await prisma.game.findUnique({
+    where: {
+      joinCode,
+    },
+    include: {
+      heroes: true,
+    },
+  });
+  if (game === null) {
+    throw new Error('Invalid room code provided!');
+  }
+  const bingo = await prisma.bingo.findUnique({
+    where: {
+      id: game.bingoId,
+    },
+    include: {
+      superpowers: true,
+    },
+  });
+  if (bingo === null) {
+    throw new Error('Something went wrong! Please get a new room code.');
+  }
+  const hasExistingUserWithSameName = await prisma.superhero.count({
+    where: {
+      name,
+      gameId: game.id,
+    },
+  });
+
+  if (hasExistingUserWithSameName > 0) {
+    throw new Error('This name is already used! Try another name!');
+  }
+
+  const user = await prisma.superhero.create({
+    data: {
+      name,
+      gameId: game.id,
+    },
+  });
+  game.heroes.push(user);
+
+  const tokenPayload = {
+    userId: user.id,
+    gameId: game.id,
+    name,
+  };
+  const token = sign(tokenPayload, process.env.JWT_SECRET!, {
+    expiresIn: '7d',
+  });
+
+  return { game, bingo, user, token };
 };
--- server/src/socket/game.ts
+++ server/src/socket/game.ts
@@ -1,9 +1,19 @@
-import { ERROR_CREATE_GAME, RES_CREATE_GAME } from 'shared';
+import {
+  ERROR_CREATE_GAME,
+  ERROR_JOIN_GAME,
+  NOTIF_JOINED_GAME,
+  RES_CREATE_GAME,
+  RES_JOIN_GAME,
+} from 'shared';
 import { Server, Socket } from 'socket.io';
 
 import * as GameController from 'controllers/gameController';
 
 type CreateGameFunction = (ownerCode: string) => Promise<void>;
+type JoinGameFunction = (data: {
+  joinCode: string;
+  name: string;
+}) => Promise<void>;
 
 export const socketCreateGame = (
   _io: Server,
@@ -15,9 +25,34 @@ export const socketCreateGame = (
 
     try {
       const game = await GameController.createGame(ownerCode);
+      socket.join(`room-${game.id}`);
       socket.emit(RES_CREATE_GAME, game);
     } catch (error) {
       socket.emit(ERROR_CREATE_GAME, error.message);
+    }
+  };
+};
+
+export const socketJoinGame = (
+  io: Server,
+  socket: Socket
+): JoinGameFunction => {
+  return async (data: { joinCode: string; name: string }): Promise<void> => {
+    console.log('REQ_JOIN_GAME');
+    console.log('Join code received: ', data.joinCode);
+    console.log('Name received: ', data.name);
+
+    try {
+      const { game, bingo, user, token } = await GameController.joinGame(
+        data.joinCode,
+        data.name
+      );
+      socket.join(`room-${game.id}`);
+      socket.emit(RES_JOIN_GAME, { game, bingo, user, token });
+      // Notify all current users that someone joined
+      socket.to(`room-${game.id}`).emit(NOTIF_JOINED_GAME, user);
+    } catch (error) {
+      socket.emit(ERROR_JOIN_GAME, error.message);
     }
   };
 };
