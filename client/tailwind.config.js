9506ac3dc83929ac8052b2bc3ea86b97634c5dc8
Hanming Zhu <hanming@huawen.com>
2021-08-09 13:23:54 +0800

    fix: fix bugs in production

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,19 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    safelist: [
+      'grid-cols-1',
+      'grid-cols-2',
+      'grid-cols-3',
+      'grid-cols-4',
+      'grid-cols-5',
+      'grid-cols-6',
+      'grid-cols-7',
+      'grid-cols-8',
+      'grid-cols-9',
+      'grid-cols-10',
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
