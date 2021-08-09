86b90f6cc1ed0690ec50c51d8def8465da1000cc
Hanming Zhu <hanming@huawen.com>
2021-08-09 11:54:43 +0800

    fix: fix ecosystem config issue

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "superhero-bingo",
-      script: "./server/src/index.js",
+      script: "./index.js",
       env: {
         NODE_ENV: "production",
       },
